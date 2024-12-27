f9bb09c610c38f27483f9b13809030162682ebc7
Bruno Pansani <bruno.pansani@nubank.com.br>
2024-12-27 00:44:35 -0300

    Add webhook logic

--- src/pages/api/webhooks/prediction.ts
+++ src/pages/api/webhooks/prediction.ts
@@ -1,13 +1,57 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import s3Client from "@/core/clients/s3";
+import db from "@/core/db";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { id, status, output } = req.body;
 
-    // Handle the webhook data here
-    console.log(`Prediction ID: ${id}`);
-    console.log(`Status: ${status}`);
-    console.log(`Output: ${output}`);
+    console.log(`Body: ${req.body}`);
+
+    // Query the database for the prediction ID on the Shots table
+
+    const prediction = await db.shot.findFirst({
+      where: {
+        replicateId: id,
+      },
+    });
+
+    console.log(`Prediction: ${prediction}`);
+
+    if (!prediction) {
+      res.status(404).json({ message: 'Prediction not found' });
+      return;
+    }
+
+    // Get the output URL from the webhook data
+    const outputUrl = output?.outputUrl;
+
+    // Download the file, upload it to S3, and update the Shot record with the S3 URL
+    if (outputUrl) {
+      const file = await fetch(outputUrl);
+      const buffer = await file.arrayBuffer();
+      const extension = outputUrl.split('.').pop();
+      const key = `${id}.${extension}`;
+      const s3Url = `https://${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com/${key}`;
+
+      await s3Client.send(
+        new PutObjectCommand({
+          Bucket: process.env.S3_UPLOAD_BUCKET!,
+          Key: key,
+          Body: Buffer.from(buffer),
+        })
+      );
+
+      let shot = await db.shot.update({
+        where: { id: prediction.id },
+        data: {
+          outputUrl: s3Url || null,
+        },
+      });
+
+      console.log(`Shot: ${shot}`);
+    }
 
     // Respond with a 200 status to acknowledge receipt of the webhook
     res.status(200).json({ message: 'Webhook received' });
