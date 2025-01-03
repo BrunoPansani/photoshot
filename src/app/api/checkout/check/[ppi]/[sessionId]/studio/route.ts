810b79f96840c9a919dcadaf00ead694494db777
Bruno Pansani <bruno.pansani@nubank.com.br>
2025-01-03 19:28:29 -0300

    Enable buy event

--- src/app/api/checkout/check/[ppi]/[sessionId]/studio/route.ts
+++ src/app/api/checkout/check/[ppi]/[sessionId]/studio/route.ts
@@ -1,6 +1,7 @@
 import db from "@/core/db";
 import { stripe } from "@/lib/stripe";
 import { NextResponse } from "next/server";
+import { sendGAEvent } from '@next/third-parties/google' 
 
 export async function GET(
   req: Request,
@@ -19,6 +20,8 @@ export async function GET(
       where: { id: ppi },
       data: { stripePaymentId: session.id },
     });
+
+    sendGAEvent('event', 'conversion', { send_to: 'AW-1037086655/p--ACKb_6eADEL_fwu4D', 'transaction_id': session.id})
 
     return NextResponse.json(
       {
